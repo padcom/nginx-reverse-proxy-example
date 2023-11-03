1cb70672ba979acabab946849b5ec94d7209efc2
Matthias Hryniszak <padcom@gmail.com>
2023-11-03 01:52:34 +0100

    Added test for serving static content with nginx

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --name=nginx --rm --network=host -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro nginx
+docker run --name=nginx --rm --network=host \
+  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v $(pwd)/index.html:/var/www/index.html:ro \
+  nginx
--- run-test.sh
+++ run-test.sh
@@ -4,3 +4,5 @@ echo "Testing node.js server directly (10 seconds)..."
 siege http://localhost:9000 -t 10s
 echo "Testing node.js server through nginx reverse proxy (10 seconds)..."
 siege http://localhost:9001 -t 10s
+echo "Testing nginx serving static content (10 seconds)..."
+siege http://localhost:9002 -t 10s
